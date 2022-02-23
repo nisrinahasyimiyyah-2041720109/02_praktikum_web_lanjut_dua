0c58584005a4770c7f7a7d316cd5215cc83396c2
nisrinahasyimiyyah-2041720109 <hasyimiyyahnisrina@gmail.com>
2022-02-23 16:56:00 +0700

    Menambahkan Controller Baru

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', [PageController::class, 'index']);
 
 Route::get('/about', [PageController::class, 'about']);
 
 Route::get('/articles/{id}', [PageController::class, 'articles']);
+*/
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
